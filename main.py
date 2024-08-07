dd0bc787d0b5aac0d901013d0b47c6c28775f474
threezinedine <threezinedine@gmail.com>
2024-08-07 09:11:48 +0700

    simple configure with separtion of development and production configuration in the development phase, the development configuration will override the production configuration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-
+import uvicorn
+import argparse
+import json
 
 app = FastAPI()
 
@@ -10,9 +12,24 @@ async def root():
 
 
 if __name__ == "__main__":
-    import uvicorn
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dev",
+        "-D",
+        action="store_true",
+        help="Run in development mode",
+    )
+    args = parser.parse_args()
+
+    config = json.load(open("config.json"))
+    devConfig = json.load(open("config.development.json"))
+
+    host = devConfig["host"] if args.dev else config["host"]
+    port = devConfig["port"] if args.dev else config["port"]
+
     uvicorn.run(
-        app='main:app',
-        host="localhost",
-        port=8000,
+        app="main:app",
+        host=host,
+        port=port,
+        reload=args.dev,
     )
