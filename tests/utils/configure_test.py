24e8e824e2b487acb0d3ecb3b0b541334cdda179
threezinedine <threezinedine@gmail.com>
2024-08-08 09:07:16 +0700

    add the conftest.py file for solving the problem of the import path add the configure feature to dynamically config the server without restarting

--- tests/utils/configure_test.py
+++ tests/utils/configure_test.py
@@ -1,6 +1,77 @@
 import unittest
+from utils.configure import Configure
+import os
+import json
 
 
 class ConfigureTest(unittest.TestCase):
-    def test_configure(self):
-        self.assertTrue(True)
+    testConfigFile = "test-config.json"
+    testConfigData = {
+        "host": "localhost",
+        "port": 8080,
+    }
+    nonExistedConfigFile = "non-existed-config.json"
+
+    testOverideConfigFile = "test-override-config.json"
+    testOverrideConfigData = {"host": "0.0.0.0"}
+
+    def setUp(self) -> None:
+        with open(self.testConfigFile, "w") as file:
+            json.dump(self.testConfigData, file)
+
+        with open(self.testOverideConfigFile, "w") as file:
+            json.dump(self.testOverrideConfigData, file)
+
+        self.validConfigure = Configure(self.testConfigFile)
+        self.validConfigure.Load()
+
+        self.overrideConfigure = Configure(self.testOverideConfigFile)
+        self.overrideConfigure.Load()
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.testConfigFile):
+            os.remove(self.testConfigFile)
+
+        if os.path.exists(self.testOverideConfigFile):
+            os.remove(self.testOverideConfigFile)
+
+    def test_GivenConfigure_WhenLoadThatConfigure_ThenTheConfigureIsLoaded(self):
+        # Act
+        self.validConfigure.Load()
+
+        # Assert
+        self.assertEqual(self.validConfigure["host"], self.testConfigData["host"])
+        self.assertEqual(self.validConfigure["port"], self.testConfigData["port"])
+        self.assertIsNone(self.validConfigure["non-existed-key"])
+
+    def test_GivenConfigure_WhenGetItem_ThenReturnTheValue(self):
+        # Arrange
+        # Act
+        host = self.validConfigure.Get("host")
+
+        # Assert
+        self.assertEqual(host, self.testConfigData["host"])
+
+    def test_GivenConfigure_WhenGetNonExistedItemWithDefaultValue_ThenReturnTheDefaultValue(
+        self,
+    ) -> None:
+        # Arrange
+        defaultValue = "default-value"
+
+        # Act
+        nonExistedItem = self.validConfigure.Get("non-existed-item", defaultValue)
+
+        # Assert
+        self.assertEqual(nonExistedItem, defaultValue)
+
+    def test_GivenConfigure_WhenIsOverridenByAnotherConfigure_ThenTheValueIsOverridden(
+        self,
+    ):
+        # Act
+        self.validConfigure.OverridenBy(self.overrideConfigure)
+
+        # Assert
+        self.assertEqual(
+            self.validConfigure["host"], self.testOverrideConfigData["host"]
+        )
+        self.assertEqual(self.validConfigure["port"], self.testConfigData["port"])
---
+++ utils/configure.py
@@ -0,0 +1,36 @@
+from typing import Optional, TypeVar
+import os
+import json
+
+T = TypeVar("T")
+
+
+class Configure:
+    def __init__(self, configFile: str) -> None:
+        self.__configFile: str = configFile
+        self.__overridenConfigure: Optional["Configure"] = None
+        self.__data: dict = {}
+
+    def Load(self) -> None:
+        if not os.path.exists(self.__configFile):
+            return
+
+        with open(self.__configFile, "r") as file:
+            self.__data = json.loads(file.read())
+
+    def Get(self, key: str, defaut: Optional[T] = None) -> Optional[T]:
+        return self[key] if self.Contains(key) else defaut
+
+    def Contains(self, key: str) -> bool:
+        return key in self.__data
+
+    def __getitem__(self, key: str) -> Optional[object]:
+        if self.__overridenConfigure and self.__overridenConfigure.Contains(key):
+            return self.__overridenConfigure[key]
+        return self.__data.get(key)
+
+    def OverridenBy(self, overrideConfigure: "Configure") -> None:
+        self.__overridenConfigure = overrideConfigure
+
+    def __repr__(self) -> str:
+        return f"<Configure data={json.dumps(self.__data)} />"
